d293a94ab484b6de6792e4efae6e47fc67a24686
Waffalz <ethantran7@yahoo.com>
2018-01-27 17:01:50 -0600

    Init Commit
    The big stuff

---
+++ LeapGestureController/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Leap;
+
+namespace LeapGestureController
+{
+    static class Program
+    {
+        public const int FRAMES_PER_SECOND = 60;
+        public static Boolean isRunning = true;
+
+
+        static void Main(string[] args)
+        {
+            Controller cont = new Controller();
+
+
+        }
+
+        public static void Update(float time)
+        {
+            while (isRunning)
+            {
+
+            };
+        }
+
+        public static void BeginLooping()
+        {
+
+        }
+    }
+    
+}
