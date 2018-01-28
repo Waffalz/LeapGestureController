5282adf21efb88bd1338e8c86c51e6ebdfd82b3f
Waffalz <ethantran7@yahoo.com>
2018-01-27 18:09:45 -0600

    Basic stuff

--- LeapGestureController/Program.cs
+++ LeapGestureController/Program.cs
@@ -4,34 +4,75 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using System.Threading;
+using System.Diagnostics;
+
 using Leap;
 
 namespace LeapGestureController
 {
     static class Program
     {
-        public const int FRAMES_PER_SECOND = 60;
-        public static Boolean isRunning = true;
+        public const int FRAMES_PER_SECOND = 1000; //the amount of update cycles per second. The higher the number, the greater the precision
+        private static Boolean isRunning = true; //whether the update loop is running
 
+        private static Stopwatch timer; //var for time elapsed
+
+        private static Thread loopThread;
+
+        private static Controller cont;
 
         static void Main(string[] args)
         {
-            Controller cont = new Controller();
+            cont = new Controller();
 
+            loopThread = new Thread(BeginLoop);
+            loopThread.Start();
 
+            Thread.Sleep(10 * 1000);
+
+            StopLoop();
+            
         }
 
-        public static void Update(float time)
+        /**
+         * Is called to several times per second to fetch the state of the controller
+         * 
+         */
+        public static void Update(TimeSpan time)
         {
+            Frame currentFrame = cont.Frame();
+
+            Console.WriteLine(time.Milliseconds);
+        }
+
+        /**
+         * Initializes the update loops and adjusts time accordingly
+         */
+        public static void BeginLoop()
+        {
+            isRunning = true;
+            timer = new Stopwatch();
+            timer.Start();
+
             while (isRunning)
             {
-
+                timer.Stop();
+                TimeSpan elapsedTime = timer.Elapsed; //stop the timer and record the amount of time passed
+                Update(elapsedTime); //call to Update and pass the amount of time since the last call to Update
+                Thread.Sleep(1000/FRAMES_PER_SECOND -  elapsedTime.Milliseconds); //wait out the remaining time for this update cycle
+                timer.Start();
             };
+
+            timer.Stop();
         }
 
-        public static void BeginLooping()
+        /**
+         * Stops the loop
+         */
+        public static void StopLoop()
         {
-
+            isRunning = false;
         }
     }
     
